9a0deaa57d8be4480243c1fe9df24fd569ee5801
Barbara Schvuchov <schvuchov.k@gmail.com>
2024-02-08 23:36:07 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import './App.css';
+
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+
+//pages
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <div className="container">
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/about' element={<About/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/firebase/config.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore} from 'firebase/firebase'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA-fHwt6joSZopQjgkmgSeqCht-ontiIYk",
+  authDomain: "miniblog-ea37d.firebaseapp.com",
+  projectId: "miniblog-ea37d",
+  storageBucket: "miniblog-ea37d.appspot.com",
+  messagingSenderId: "186785155887",
+  appId: "1:186785155887:web:1f453745c4db3a05abefda"
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+const db = getFirestore(app)
+
+export { db };
